9b5a38a5ac8603331007b9d6dfce68e1feb909b2
Angel Rios <riosmartinezkg@gmail.com>
2026-01-24 15:09:30 -0600

    Feat: optional formatId add in download-videos.js

--- controllers/download-video.js
+++ controllers/download-video.js
@@ -4,14 +4,14 @@ const path = require("path");
 const { fromInfo } = require('../helpers/youtubedl')
 const downloadVideo = async (req, res) => {
 
-    const { outputPath, filename } = req.body;
+    const { outputPath, filename, infoId, formatId} = req.body;
 
     const finalName = filename || `video_${Date.now()}.mp4`;
     const downloadUserPath = path.join(outputPath, finalName);
 
 
-    await fromInfo('./videoInfo.json', {
-        format: '18',
+    await fromInfo( `./tmp/${infoId}.json` , {
+        format: `${formatId}`,
         output: `${downloadUserPath}`
     })
     console.log('descargando video ')

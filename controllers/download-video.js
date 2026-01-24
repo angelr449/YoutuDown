996d78981775df0cce0e6f6d83fe8d795908946f
Angel Rios <riosmartinezkg@gmail.com>
2026-01-24 14:39:56 -0600

    Feat: basic video download completed, download-video.js

--- controllers/download-video.js
+++ controllers/download-video.js
@@ -1,12 +1,15 @@
+
+const path = require("path");
+
 const { fromInfo } = require('../helpers/youtubedl')
-const  downloadVideo = async(req, res) => {
-    
+const downloadVideo = async (req, res) => {
 
-    
-  const finalName = filename || `video_${Date.now()}.mp4`;
-  const downloadUserPath = path.join(outputPath, finalName);
+    const { outputPath, filename } = req.body;
 
-    
+    const finalName = filename || `video_${Date.now()}.mp4`;
+    const downloadUserPath = path.join(outputPath, finalName);
+
+
     await fromInfo('./videoInfo.json', {
         format: '18',
         output: `${downloadUserPath}`

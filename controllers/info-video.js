397cf4504a2bd998c042cb40bf46b3946dffd4a7
Angel Rios <riosmartinezkg@gmail.com>
2026-01-23 22:42:37 -0600

    feat(info-video): simplify formats output

--- controllers/info-video.js
+++ controllers/info-video.js
@@ -2,36 +2,53 @@ const { getInfo, fromInfo } = require('../helpers/youtubedl')
 const fs = require('fs')
 
 
-const infoVideo = async(req, res) => {
+const infoVideo = async (req, res) => {
+    const { url } = req.body;
 
-    let url = req.url;
     // let url = 'https://youtu.be/z0hWGUfxbbg?si=z-W7Tv9ttfi-DG52'
 
 
-    
+    try {
+        // with this function we get a YtResponse with all the info about the video
+        // this info can be read and used and then passed again to youtube-dl, without having to query it again
+        const info = await getInfo(url)
 
-    
+        // write the info to a file for youtube-dl to read it
+        fs.writeFileSync('./videoInfo.json', JSON.stringify(info))
 
 
-    // with this function we get a YtResponse with all the info about the video
-    // this info can be read and used and then passed again to youtube-dl, without having to query it again
-    const info = await getInfo(url)
+        // the info the we retrive can be read directly or passed to youtube-dl
 
-    // write the info to a file for youtube-dl to read it
-    fs.writeFileSync('./videoInfo.json', JSON.stringify(info))
-    
 
-    // the info the we retrive can be read directly or passed to youtube-dl
-    
-    
+        // Fomats Map
 
-    
-    
-    res.json({
-        title: info.title,
-        duration: info.duration,
-        formats: info.formats.length
-    })
+        const simplifiedFormats = info.formats.map(f => ({
+            id: f.format_id,
+            ext: f.ext,
+            resolution: f.format_note || `${f.height || 0}p`,
+            hasAudio: f.acodec && f.acodec !== 'none',
+            hasVideo: f.vcodec && f.vcodec !== 'none'
+        }));
+
+
+
+
+
+        res.json({
+            title: info.title,
+            duration: info.duration,
+            formats: simplifiedFormats
+        })
+    } catch (err) {
+
+        res.status(500).json({ error: err.messege });
+
+    }
+
+
+
+
+
 }
 
 
--- routes/you-down.js
+++ routes/you-down.js
@@ -2,10 +2,17 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { infoVideo } = require('../controllers');
 const { downloadVideo } = require('../controllers/download-video');
+const { validateYouTubeUrl } = require('../middlewares');
+const { requestValidation } = require('../middlewares/requestValidation');
 
 const router = Router();
 
-router.get('/info', infoVideo);
+router.post('/info', [
+    check('url', 'URL is required').not().isEmpty(),
+    validateYouTubeUrl,
+    requestValidation,
+
+],infoVideo);
 
 
 router.get('/download', downloadVideo);

abac4be5bf876fa52b35d4722ce68954ae925096
Angel Rios <riosmartinezkg@gmail.com>
2026-01-23 21:46:36 -0600

    feat: add validate-youtube-url meddleware

---
+++ middlewares/index.js
@@ -0,0 +1,11 @@
+
+const requestValidation = require('./validate-youtube-url');
+const validateYouTubeUrl = require('./validate-youtube-url');
+
+
+
+module.exports = {
+    ...requestValidation,
+    ...validateYouTubeUrl
+
+}

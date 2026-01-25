71a1ff32031adfd697817de20056f2f62b0dc04e
Angel Rios <riosmartinezkg@gmail.com>
2026-01-24 20:55:07 -0600

    Fixed latest issues and documentation completed

--- middlewares/requestValidation.js
+++ middlewares/requestValidation.js
@@ -1,5 +1,15 @@
 const { validationResult } = require('express-validator');
 
+const { Request, Response, NextFunction } = require('express');
+
+/**
+ * Middleware to validate incoming requests using express-validator.
+ *
+ * @param {Request} req - The Express request object.
+ * @param {Response} res - The Express response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Response|undefined} - Returns a 400 response with validation errors if any; otherwise calls next().
+ */
 const requestValidation = (req, res, next ) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {

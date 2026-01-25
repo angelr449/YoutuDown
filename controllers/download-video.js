39c9447e6597d4a29a8ffb5ca9a940e41baf8ada
Angel Rios <riosmartinezkg@gmail.com>
2026-01-24 20:18:57 -0600

    Fixed mistake Git 1

--- controllers/download-video.js
+++ controllers/download-video.js
@@ -1,28 +1,48 @@
 
 const path = require("path");
 
-const { fromInfo, getInfo } = require('../helpers/youtubedl');
+
 const { downloadWithProgress } = require("../helpers/progress-bar");
+
 const downloadVideo = async (req, res) => {
 
-    const { outputPath, filename, infoId, formatId } = req.body;
+    const { outputPath, filename, infoId, formatId} = req.body;
 
+
+
+    
+
+
+
+    
+    
+
+    // Final Path
     const finalName = filename || `video_${Date.now()}.mp4`;
     const downloadUserPath = path.join(outputPath, finalName);
 
 
-    await fromInfo(`./tmp/${infoId}.json`, {
+    // Fast answer
+    res.json({ messege: "download video funcionando" });
+
+    
+
+
+
+    // await fromInfo(`./tmp/${infoId}.json`, {
+    //     format: `${formatId}`,
+    //     output: `${downloadUserPath}`
+    // });
+
+    // Init download and progress-bar
+
+    await downloadWithProgress(`./tmp/${infoId}.json`, {
         format: `${formatId}`,
         output: `${downloadUserPath}`
     });
 
-    await downloadWithProgress(`./tmp/${infoId}.json`, {
-        format: formatId,
-        output: downloadUserPath
-    });
 
-
-    res.json({ messege: "download video funcionando" });
+    
 }
 
 

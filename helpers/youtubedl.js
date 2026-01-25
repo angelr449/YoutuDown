b2c4c55dfc86c946fabfbdb55939ecd82bd4e094
Angel Rios <riosmartinezkg@gmail.com>
2026-01-24 20:09:32 -0600

    First part to documentation

--- helpers/youtubedl.js
+++ helpers/youtubedl.js
@@ -1,6 +1,11 @@
+const youtubedl = require('youtube-dl-exec') //library to download videos 
 
-const youtubedl = require('youtube-dl-exec')
-
+/**
+ * Fetches information about a YouTube video or playlist.
+ * @param {URL | string} url - The URL fo the YouTube video of playlist.
+ * @param {Object} [flasg={}] - Optional flasg to pass to youtubedl. 
+ * @returns {Promise<Object>} - A promise that resolves with the video.
+ */
 const getInfo = (url, flags = {}) => {
     return youtubedl(url, {
         dumpSingleJson: true,
@@ -8,6 +13,14 @@ const getInfo = (url, flags = {}) => {
     })
 }
 
+
+/**
+ * Loads YouTube video or playlist information from a JSON info file.
+ *
+ * @param {string} infoFile - Path to the JSON file containing video information.
+ * @param {Object} [flags={}] - Optional flags to pass to youtubedl.exec.
+ * @returns {Promise<Object>} - A promise that resolves with the video information loaded from the file.
+ */
 const fromInfo = (infoFile, flags = {}) => {
     return youtubedl.exec('', {
         loadInfoJson: infoFile,
--- models/server.js
+++ models/server.js
@@ -2,18 +2,17 @@ const express = require('express');
 const cors = require('cors');
 
 
-// const { socketController } = require('../sockets/controller');
 
 class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
-        this.server = require('http').createServer(this.app);
-        // this.io = require('socket.io')(this.server)
-
+        this.port = process.env.PORT; // Port Server, example: 8080
+        this.server = require('http').createServer(this.app); 
+        
+        // Paths (route)
         this.paths = {
-            youtuDown: '/api/youtuDown'
+            youtuDown: '/api/youtuDown' // This like: localhost:8080/api/youtuDown
         };
 
         
@@ -24,8 +23,7 @@ class Server {
         // Application routes
         this.routes();
 
-        // Sockets
-        // this.sockets();
+        
     }
 
 
@@ -48,12 +46,7 @@ class Server {
         this.app.use(this.paths.youtuDown, require('../routes/youtu-down'));
     }
 
-    // sockets() {
-    //     this.io.on('connection', (socket) =>
-    //         socketController(socket, this.io)
-    //     );
-    // }
-
+    
     listen() {
         this.server.listen(this.port, () => {
             console.log('Server running on port', this.port);
